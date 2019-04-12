ae6a15254c130b9fde1c7ed705620b595cc44bfd
debochinya <49538757+debochinya@users.noreply.github.com>
2019-04-12 01:43:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 
+let request = require('request');
+
+let apiKey = 'e7b05c3b9eb830ce3696c5eeaa8cea57';
+let city = 'London,uk';
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+
+request(url, function (err, response, body) {
+  if(err){
+    console.log('error:', error);
+  } else {
+    console.log('body:', body);
+  }
+});
